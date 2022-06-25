ea2f70989a659ef90c16d2cf322e8570f33ad282
Christian Caique <christian.caique@hotmail.com>
2022-06-25 13:35:49 -0300

    adiciona views, partial0s e bootstrap

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs'); // Estou dizendo para o Express usar o EJS como View engine
+app.use(express.static('public')); // Usamos para dizer aonde estão nossos arquivos staticos do nosso projeto
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
+
+app.listen(3000, () => console.log("Rodando"))
