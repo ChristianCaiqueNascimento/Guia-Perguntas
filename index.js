5e4f262b2ee2418e336fec60f8a0a4475378314b
Christian Caique <christian.caique@hotmail.com>
2022-06-29 15:24:08 -0300

    configurando conexão com o banco

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+connection
+.authenticate()
+.then(() => {
+    console.log("Conexão feita com o banco");
+})
+.catch((msgErro) => {
+    console.log(msgErro);
+})
 
 app.set('view engine', 'ejs'); // Estou dizendo para o Express usar o EJS como View engine
 app.use(express.static('public')); // Usamos para dizer aonde estão nossos arquivos staticos do nosso projeto
